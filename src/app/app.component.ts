7f2f76acf488739a4e1563547e9a4da6d5097260
Ambati-Rajani <ambatirajini2003@gmail.com>
2024-11-08 15:15:00 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MindComponent } from './mind/mind.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { FoodDetailComponent } from './food-detail/food-detail.component';
+import {  HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,MindComponent,RestaurantsComponent,RestaurantDetailComponent,FoodDetailComponent,HttpClientModule,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
